6964fb129557cc58934440db4c083e6eaa64e9d2
Alexis Frank <time2emit@gmail.com>
2017-07-09 09:50:07 -0700

    commit

---
+++ keys.js
@@ -0,0 +1,12 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '8ZPwieiNEyHNVGZk23MBcyrPf',
+  consumer_secret: 'EeBl5MIybamK4bMdlPz6osoXLrxrNBNzeXtpH4M3uUsd76YDIg',
+  access_token_key: '883431169315885057-mgyxRRxQOfhFiTynfWEwImCgv3UdQi4',
+  access_token_secret: 'gPvA2Cocqn5mYRFoQp7NNf9FOWTc7ATwAE2MeLcnRh6fz',
+}
+
+//Spotify
+//Client ID e6050cbf903d4d50beb1ddc8ab3c6642
+//Client Secret e7f701f0ee274816a7ccbc3375ffee34
---
+++ liri.js
@@ -0,0 +1,7 @@
+//require the filesystem package for node have the ability to read from and write to other files
+fs = require("fs");
+
+//grab keys from keys.js and save it in a variable
+
+keys = require("./keys.js");
+
